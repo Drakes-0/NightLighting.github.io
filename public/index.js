e90f92189a9653a5773413c98b0c910141a509a0
wujiang <5569082@qq.com>
2019-08-30 19:23:55 +0800

    upload

--- public/index.js
+++ public/index.js
@@ -22,6 +22,15 @@
     var BUFFER_SIZE = 4096;
     var CHANNEL_COUNT = 2;
     var SOUND_CHANNELS = [[], []];
+    var GENDER_MALE_LABEL = ['/m/05zppz', '/t/dd00003'];
+    var GENDER_FEMALE_LABEL = ['/m/02zsn', '/t/dd00004'];
+    var AGE_ADULT_LABEL = ['/m/05zppz', '/m/02zsn', '/t/dd00003', '/t/dd00004'];
+    var GENDER_NEUTRAL_LABEL = ['/m/0ytgt', '/m/0261r1', '/t/dd00135', '/t/dd00001', '/t/dd00002', '/t/dd00005', '/t/dd00013'];
+    var AGE_NONAGE_LABEL = ['/m/0ytgt', '/m/0261r1', '/t/dd00135', '/t/dd00001', '/t/dd00002', '/t/dd00005', '/t/dd00013'];
+    var EMOTION_ANGER_LABEL = ['/m/07p6fty', '/m/07q4ntr', '/t/dd00135', '/m/03qc9zr', '/m/07qf0zm', '/m/0ghcn6'];
+    var EMOTION_EXCITED_LABEL = ['/m/07p6fty', '/m/07rwj3x', '/m/04gy_2', '/t/dd00135', '/m/03qc9zr', '/m/053hz1', '/m/028ght'];
+    var EMOTION_LAUGH_LABEL = ['/m/01j3sz', '/t/dd00001', '/m/07r660_', '/m/07s04w4', '/m/07sq110', '/m/07rgt08'];
+    var EMOTION_CRY_LABEL = ['/m/0463cq4', '/t/dd00002', '/m/07qz6j3'];
     var granted = false;
     var startTime = null;
     var audioCtx = null;
@@ -89,7 +98,7 @@
     var mergePCM = function () {
         var left = mergeChannel(SOUND_CHANNELS[0]);
         var right = mergeChannel(SOUND_CHANNELS[1]);
-        var length = left.length;
+        var length = Math.floor(left.length / 44100) * 44100;
         var data = new Float32Array(length * 2);
         for (var i = 0; i < length; i++) {
             var j = i * 2;
@@ -110,14 +119,14 @@
         var buffer = new ArrayBuffer(TOTAL_SIZE);
         var view = new DataView(buffer);
         writeUTFBytes(view, 0, 'RIFF');
-        view.setUint32(4, TOTAL_SIZE, true);
+        view.setUint32(4, TOTAL_SIZE - 8, true);
         writeUTFBytes(view, 8, 'WAVE');
         writeUTFBytes(view, 12, 'fmt ');
         view.setUint32(16, 16, true);
         view.setUint16(20, 1, true);
         view.setUint16(22, 2, true);
         view.setUint32(24, 44100, true);
-        view.setUint32(28, 44100 * 2, true);
+        view.setUint32(28, 44100 * 2 * 2, true);
         view.setUint16(32, 2 * 2, true);
         view.setUint16(34, 16, true);
         writeUTFBytes(view, 36, 'data');
@@ -137,12 +146,24 @@
         var xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function () {
             if (xhr.readyState === 4) {
-                console.log(xhr.responseText);
+                callback(xhr.responseText);
             }
         };
         xhr.open('POST', url, true);
         xhr.setRequestHeader('accept', 'application/json');
         xhr.send(formData);
+    };
+    var predictResultParseHandle = function (result) {
+        try {
+            var predictResult = JSON.parse(result).predictions;
+            var sex = [];
+            var age = [];
+            var emotion = [];
+            var background = [];
+            var weight = predictResult.length;
+        }
+        catch (e) {
+        }
     };
     var downloadRecord = function (file) {
         var link = document.createElement('a');
@@ -153,7 +174,7 @@
     var handleRecord = function () {
         var pcm = mergePCM();
         var wav = createFile(pcm);
-        uploadRecord(SERVER_URL, wav, function () { }, 'audio');
+        uploadRecord(SERVER_URL, wav, predictResultParseHandle, 'audio');
     };
     button.addEventListener('touchstart', function () {
         if (!granted) {
